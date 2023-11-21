ea5b47307811de95dcd7be335f6dfda568ab8272
Vero-Sampedro <verosampedro@icloud.com>
2023-11-21 21:58:47 +0100

    completed exercise

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
+const fs = require("fs/promises");
 
-/* Importo este módulo para que me devuelva promesas */
-const fs = require('fs/promises');
-
-/* Defino una función con async para indicar que puede contener operaciones asincrónicas que se van a manejar de manera sincrónica con un await posteriormente */
 async function readJSON() {
-
   try {
-    /* readFile para leer el contenido del .json y me devuelva una cadena de texto que se almacenará en la variable declarada 'data'. El await para que 
-    la promesa que me va a devolver readFile se resuelva */
-    const data = await fs.readFile('exercise.json', 'utf8');
+    const data = await fs.readFile("exercise.json", "utf8");
 
-    /* Convierto la cadena de texto almacenada en 'data' en un objeto js */
     const jsonData = JSON.parse(data);
 
-    /* Se recorre el array results del objeto jsonData y se imprime el título de cada elemento en la consola */
     jsonData.results.forEach(function (item) {
       console.log(item.title);
     });
 
-    console.log('Lectura correcta.');
-
+    console.log("Lectura correcta.");
   } catch (err) {
-    console.error('Error leyendo el archivo:', err);
+    console.error("Error leyendo el archivo:", err);
   }
 }
 
